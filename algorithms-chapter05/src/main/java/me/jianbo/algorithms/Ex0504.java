c6bd657ff30f8b074a0e4cd54fe2e263611f857f
hejianbo <hejanbo@qq.com>
2019-06-30 22:23:01 +0800

    例题5.4

---
+++ algorithms-chapter05/src/main/java/me/jianbo/algorithms/Ex0504.java
@@ -0,0 +1,14 @@
+package me.jianbo.algorithms;
+
+/**
+ * 例5.4
+ * 设计一个算法在1, 2, 3, ... , 9（顺序不变）数字之间插入 + 或 - 或什么都不插入， 使得计算结果总是100的程序，
+ * 并输出所有的可能性。
+ * <p>
+ * 例如：
+ * 1 + 2 + 34 - 5 + 67 - 8 + 9 = 100
+ */
+public class Ex0504 {
+    public static void main(String[] args) {
+    }
+}
